2d9fdfbc7859f4b4fa6f2c5f6759a3efe395d921
ws <mkwork@qq.com>
2017-06-25 20:22:51 +0800

    @SpringBootApplication and @Configuration + @ComponentScan + @EnableAutoConfiguration

--- spring-boot-web/src/main/java/controller/Example.java
+++ spring-boot-web/src/main/java/controller/Example.java
@@ -2,14 +2,22 @@ package controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by WS on 2017/6/22.
  */
+
+
 @RestController
+// @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 @EnableAutoConfiguration
+@ComponentScan
+@Configuration
 public class Example {
 
     @RequestMapping("/")
