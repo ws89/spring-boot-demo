0243e176a52f2a4ad2099e23d7fa744c03f841ca
ws <mkwork@qq.com>
2017-06-26 23:47:55 +0800

    customizing SpringApplication

--- spring-boot-web/src/main/java/controller/Example.java
+++ spring-boot-web/src/main/java/controller/Example.java
@@ -1,5 +1,6 @@
 package controller;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,7 +28,21 @@ public class Example {
 
 
     public static void main(String[] args){
+
+      //  startup1(args);
+
+        startup2(args);
+    }
+
+    static void startup1(String[] args){
         SpringApplication.run(Example.class,args);
+    }
+
+    static void startup2(String[] args){
+        SpringApplication springApplication = new SpringApplication(Example.class);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.run( args );
+
     }
 
 }
