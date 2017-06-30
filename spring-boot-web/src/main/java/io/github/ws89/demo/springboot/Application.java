a79f54dda1afa0ca9c3d13d1ec221d13757dae61
ws <mkwork@qq.com>
2017-06-30 13:14:44 +0800

    split

--- spring-boot-web/src/main/java/io/github/ws89/demo/springboot/Application.java
+++ spring-boot-web/src/main/java/io/github/ws89/demo/springboot/Application.java
@@ -5,25 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by WS on 2017/6/22.
  */
 
-
-@RestController
 // @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 @EnableAutoConfiguration
 @ComponentScan
 @Configuration
 public class Application {
-
-    @RequestMapping("/")
-    String home(){
-        return "Hello World! from Spring boot!";
-    }
 
 
     public static void main(String[] args){
--- spring-boot-web/src/main/java/io/github/ws89/demo/springboot/controller/ExampleController.java
+++ spring-boot-web/src/main/java/io/github/ws89/demo/springboot/controller/ExampleController.java
@@ -15,33 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 // @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
-@EnableAutoConfiguration
-@ComponentScan
-@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan
+//@Configuration
 public class ExampleController {
 
     @RequestMapping("/")
     String home(){
         return "Hello World! from Spring boot!";
-    }
-
-
-    public static void main(String[] args){
-
-      //  startup1(args);
-
-        startup2(args);
-    }
-
-    static void startup1(String[] args){
-        SpringApplication.run(ExampleController.class,args);
-    }
-
-    static void startup2(String[] args){
-        SpringApplication springApplication = new SpringApplication(ExampleController.class);
-        springApplication.setBannerMode(Banner.Mode.OFF);
-        springApplication.run( args );
-
     }
 
 }
